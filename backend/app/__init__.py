513eb26142620965d98b212e1b84d3f87706a936
mjr-portfolio <cyd.miro@gmail.com>
2025-12-01 22:53:21 +0000

    Adding print to check why SQLAlchemy isnt working

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,9 +11,11 @@ login_manager = LoginManager()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    
+
     # Load default config
     app.config.from_object('app.config.Config')
+
+    print("RAW DB URI:", app.config.get("SQLALCHEMY_DATABASE_URI"))
 
     # Normalize Postgres URLs (important for production hosts)
     uri = app.config.get("SQLALCHEMY_DATABASE_URI")
